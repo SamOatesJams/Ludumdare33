9fe782c56322d59d803f6609435967795d0d1f3d
samoatesgames <sam@samoatesgames.com>
2015-08-22 20:56:11 +0100

    Add animations to player and remote players.

---
+++ Assets/Resources/Scripts/Client/Player/PlayerAnimationController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Realms.Client.Player
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class PlayerAnimationController : MonoBehaviour
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private NavMeshAgent m_navAgent = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Animator m_animator = null;
+
+        void Start()
+        {
+            m_navAgent = this.GetComponent<NavMeshAgent>();
+            m_animator = this.GetComponentInChildren<Animator>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void Update()
+        {
+            bool isWalking = false;
+
+            if (m_navAgent.velocity.sqrMagnitude > float.Epsilon)
+            {
+                isWalking = true;
+            }
+
+            m_animator.SetBool("IsWalking", isWalking);
+        }
+    }
+}
