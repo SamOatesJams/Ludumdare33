41914620145953a11e21ae966a7fd8c75b493a1d
James Fitzpatrick <thefishlive@hotmail.com>
2015-08-22 10:34:55 +0100

    Added movement functionality

---
+++ Assets/Resources/Scripts/Client/Player/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using InControl;
+using System.Collections;
+
+namespace Realms.Client.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public Camera Camera;
+
+        private Vector3 lastLocation;
+        private NavMeshAgent navAgent;
+        private PlayerControls playerControls;
+
+        // Use this for initialization
+        void Start()
+        {
+            navAgent = GetComponent<NavMeshAgent>();
+
+            playerControls = new PlayerControls();
+            playerControls.SetupControls();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (playerControls.MouseLeft.IsPressed)
+            {
+                Debug.Log("Mouse clicked");
+                RaycastHit hit;
+                Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+                Physics.Raycast(ray, out hit);
+
+                if (hit.collider)
+                {
+                    Debug.Log("Moving to");
+                    navAgent.destination = hit.point;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Client/Player/PlayerControls.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using InControl;
+using System.Collections;
+
+namespace Realms.Client.Player
+{
+    public class PlayerControls : PlayerActionSet
+    {
+        public PlayerAction MouseLeft;
+        public PlayerAction MouseRight;
+
+        public PlayerControls()
+        {
+            MouseLeft = CreatePlayerAction("Mouse Left Click");
+            MouseRight = CreatePlayerAction("Mouse Right Click");
+        }
+
+        public void SetupControls()
+        {
+            MouseLeft.AddDefaultBinding(Mouse.LeftButton);
+            MouseRight.AddDefaultBinding(Mouse.RightButton);
+        }
+    }
+}
+
+
